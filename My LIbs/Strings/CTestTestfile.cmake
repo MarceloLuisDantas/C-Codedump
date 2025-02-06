674ab66ec594318b16fc79a4e50cf0221187f99b
marcelo luis <marceloluis257gh@gmail.com>
2025-02-06 07:48:10 -0300

    movendo libs

--- My LIbs/Strings/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/marcelo/Documentos/C/My LIbs/Strings
-# Build directory: /home/marcelo/Documentos/C/My LIbs/Strings
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
